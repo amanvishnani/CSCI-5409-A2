137ff62b2d49f8877c77c8a59e435b4511a046d8
Aman Vishnani <aman.vishnani@dal.ca>
2020-02-27 20:52:23 -0400

    Added Fibonacci Function

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+def Fibonacci(N):
+    all = []
+    f1 = 0
+    f2 = 1
+    for i in range(N):
+        if i is 0:
+            all.append(f1)
+        elif i is 1:
+            all.append(f2)
+        else:
+            temp = f1 + f2
+            f1 = f2
+            f2 = temp
+            all.append(f2)
+    print(",".join([str(k) for k in all]))
+
+fd = open('input.txt')
+for i in fd:
+    N = int(i)
+    Fibonacci(N)
